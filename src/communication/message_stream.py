ea86ab22e60826855984fa51d6b13d5c8f92cef3
Fabian Hofmann <fabian.hofmann@dai-labor.de>
2023-04-08 12:47:58 +0200

    finished debugging the non-blocking issue for unstable connections, also completed docstrings and typehints. Remaining: - enable SSL - enable compression - restructure logging - check int conversions (network/host order)

--- src/communication/message_stream.py
+++ src/communication/message_stream.py
@@ -12,12 +12,8 @@ from typing import Tuple
 
 # TODO set up SSL https://docs.python.org/3/library/ssl.html
 # TODO checkout gzip/lzma/bz2/mgzip/zipfile for compression
-# TODO bundling of socket functions
 # TODO filter logging
-# TODO filter exceptions
-# TODO fix misc FIXME
-# TODO docstrings, typehints
-# TODO ASYNC CLEANUP (STARTED FLAG) -> NON BLOCKING
+# TODO int conversions
 
 class StreamEndpoint:
     class EndpointType(Enum):
@@ -37,6 +33,15 @@ class StreamEndpoint:
 
         def __init__(self, addr: Tuple[str, int], remote_addr: Tuple[str, int], send_b_size: int, recv_b_size: int,
                      logger: logging.Logger):
+            """Creates a new socket endpoint, a bundle of up to two sockets, that is used to communicate with another
+            endpoint over a persistent TCP connection.
+
+            :param addr: Address of endpoint.
+            :param remote_addr: Address of remote endpoint to connect to.
+            :param send_b_size: Underlying send buffer size of socket.
+            :param recv_b_size: Underlying receive buffer size of socket.
+            :param logger: Logger to use.
+            """
             self._addr = addr
             self._remote_addr = remote_addr
             self._send_b_size = send_b_size
@@ -52,6 +57,9 @@ class StreamEndpoint:
             self._started = False
 
         def connect(self):
+            """Establishes a connection to the remote socket, opening the socket first and initializing the connection
+            socket if in server mode (sink). Fault-tolerant for breakdowns and resets in the connection.
+            """
             while self._started:
                 try:
                     self.close()
@@ -59,15 +67,16 @@ class StreamEndpoint:
                     if self._remote_addr is None:
                         self._sock.listen(0)
                         self._remote_sock, _ = self._sock.accept()
-                        self._remote_sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self._send_b_size)
                         self._remote_sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self._recv_b_size)
                     break
-                except (OSError, RuntimeError) as e:
+                except OSError as e:
                     self._logger.debug(f"{e.__class__.__name__}({e}) while trying to establish connection. Retrying...")
                     sleep(1)
                     continue
 
         def close(self):
+            """Closes the sockets of an endpoint, shutdowns any potential connection that might have been established.
+            """
             def close_socket(sock: socket.socket):
                 try:
                     sock.shutdown(socket.SHUT_RDWR)
@@ -79,7 +88,14 @@ class StreamEndpoint:
                 close_socket(self._remote_sock)
             close_socket(self._sock)
 
-        def send(self, p_data: bytes) -> None:
+        def send(self, p_data: bytes):
+            """Sends the given bytes of a single object over the connection, performing simple marshalling (size is
+            sent first, then the bytes of the object). Fault-tolerant for breakdowns and resets in the connection.
+            Blocking.
+
+            :param p_data: Bytes to send.
+            """
+
             # noinspection PyTypeChecker
             def send_payload(payload):
                 def send_n_data(data, size):
@@ -87,7 +103,7 @@ class StreamEndpoint:
                     while sent_bytes < size:
                         n_sent_bytes = self._sock.send(data[sent_bytes:])
                         if n_sent_bytes == 0:
-                            raise RuntimeError("Stream broken!")
+                            raise RuntimeError("Connection broken!")
                         sent_bytes += n_sent_bytes
 
                 payload_size = len(payload)
@@ -102,14 +118,23 @@ class StreamEndpoint:
                     self._logger.debug(f"{e.__class__.__name__}({e}) while trying to send data. Retrying...")
                     self.connect()
 
-        def recv(self) -> bytes:
+        def recv(self, timeout: int = None) -> bytes:
+            """Receives the bytes of a single object sent over the connection, performing simple marshalling (size is
+            sent first, then the bytes of the object). Fault-tolerant for breakdowns and resets in the connection.
+            Blocking in default-mode if timeout not set.
+
+            :param timeout: Timeout (seconds) to receive an object to return.
+            :return: Received bytes.
+            :raises TimeoutError: If timeout set and triggered.
+            """
+
             def recv_payload():
                 def recv_n_data(size, buff_size=4096):
                     data = b''
                     while size > 0:
                         n_data = self._remote_sock.recv(min(size, buff_size))
                         if n_data == b'':
-                            raise RuntimeError("Stream broken!")
+                            raise RuntimeError("Connection broken!")
                         data += n_data
                         size -= len(n_data)
                     return data
@@ -120,12 +145,22 @@ class StreamEndpoint:
 
             while self._started:
                 try:
-                    return recv_payload()
+                    self._remote_sock.settimeout(timeout)
+                    p_data = recv_payload()
+                    self._remote_sock.settimeout(None)
+                    return p_data
                 except (OSError, RuntimeError) as e:
+                    if timeout is not None and type(e) is TimeoutError:
+                        raise e
                     self._logger.debug(f"{e.__class__.__name__}({e}) while trying to receive data. Retrying...")
                     self.connect()
 
         def _open(self):
+            """Opens the main socket, binding it to a functioning address and if in client mode (source), also tries to
+            establish a connection to the remote socket. Supports hostname resolution.
+
+            :raises RuntimeError: If none of the addresses succeed to create a working socket.
+            """
             for res in socket.getaddrinfo(*self._addr, type=socket.SOCK_STREAM):
                 s_af, s_t, s_p, _, s_addr = res
 
@@ -139,8 +174,6 @@ class StreamEndpoint:
                     try:
                         self._sock = socket.socket(r_af, r_t, r_p)
                         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self._send_b_size)
-                        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self._recv_b_size)
                     except OSError:
                         self._sock = None
                         continue
@@ -152,13 +185,14 @@ class StreamEndpoint:
                         continue
                     if self._remote_addr is not None:
                         try:
+                            self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self._send_b_size)
                             self._sock.connect(r_addr)
                         except OSError:
                             self._sock.close()
                             self._sock = None
                             continue
                     return
-            raise RuntimeError(f"Could not open socket with address ({self._addr}, {self._remote_addr}")
+            raise RuntimeError(f"Could not open socket with address ({self._addr}, {self._remote_addr})")
 
         def __del__(self):
             self.close()
@@ -217,20 +251,23 @@ class StreamEndpoint:
 
         if self._endpoint_type.value == 0:
             if self._multithreading:
-                self._thread = threading.Thread(target=self._create_source)
+                self._thread = threading.Thread(target=self._create_source, name="AsyncSource")
                 self._thread.start()
             else:
                 self._create_source()
         else:
             if self._multithreading:
-                self._thread = threading.Thread(target=self._create_sink)
+                self._thread = threading.Thread(target=self._create_sink, name="AsyncSink")
                 self._thread.start()
             else:
                 self._create_sink()
 
         self._logger.debug("Endpoint started.")
 
-    def stop(self):  # FIXME thread safety!
+    def stop(self):
+        """Stops the endpoint and closes the stream, cleaning up underlying structures. If multithreading is enabled,
+        waits until the endpoint thread stops before performing cleanup.
+        """
         self._logger.debug("Stopping endpoint...")
         if not self._started:
             self._logger.debug("Endpoint has not been started!")
@@ -258,7 +295,7 @@ class StreamEndpoint:
 
         p_obj = pickle.dumps(obj)
 
-        if self._multithreading:  # FIXME BLOCKING?
+        if self._multithreading:
             self._logger.debug(
                 f"Multithreading detected, putting object into internal buffer (size={self._buffer.qsize()})...")
             self._buffer.put(p_obj)
@@ -266,11 +303,13 @@ class StreamEndpoint:
             self._endpoint_socket.send(p_obj)
         self._logger.debug(f"Pickled object sent of size {len(p_obj)}.")
 
-    def receive(self) -> object:
+    def receive(self, timeout: int = None) -> object:
         """Generic receive function that receives data as a pickle over the persistent datastream, unpickles it into the
-         respective object and returns it. Blocking.
+         respective object and returns it. Blocking in default-mode if timeout not set.
 
+        :param timeout: Timeout (seconds) to receive an object to return.
         :return: Received object.
+        :raises TimeoutError: If timeout set and triggered.
         """
         self._logger.debug("Receiving object...")
         if not self._started:
@@ -278,46 +317,57 @@ class StreamEndpoint:
         elif self._endpoint_type.value == 0:
             raise RuntimeError("Endpoint is not a sink!")
 
-        if self._multithreading:  # FIXME BLOCKING?
+        if self._multithreading:
             self._logger.debug(
                 f"Multithreading detected, retrieving object from internal buffer (size={self._buffer.qsize()})...")
-            p_obj = self._buffer.get()
+            try:
+                p_obj = self._buffer.get(timeout=timeout)
+            except queue.Empty:
+                raise TimeoutError
         else:
-            p_obj = self._endpoint_socket.recv()
+            p_obj = self._endpoint_socket.recv(timeout)
 
         self._logger.debug(f"Pickled object received of size {len(p_obj)}.")
         return pickle.loads(p_obj)
 
     def _create_source(self):
+        """Creates the streaming endpoint as a source, i.e. an endpoint that is able to send messages. By doing so, two
+        endpoints are connected and the datastream is opened. If started in multithreading mode, also starts the loop
+        to send objects from the internal buffer.
+        """
         self._logger.debug("Creating source...")
         self._endpoint_socket.connect()
         self._logger.debug("Connected to remote endpoint.")
 
         if self._multithreading:
             self._logger.debug("Multithreading enabled.")
-            while self._started:  # FIXME CHECK
+            while self._started:
                 try:
                     self._logger.debug(
                         f"Retrieving object to send from internal buffer (size={self._buffer.qsize()})...")
                     p_data = self._buffer.get(timeout=10)
                     self._endpoint_socket.send(p_data)
-                    self._logger.debug(f"Pickled object sent of size {len(p_data)}.")  # FIXME
+                    self._logger.debug(f"Pickled object sent of size {len(p_data)}.")
                 except queue.Empty:
                     pass
             self._endpoint_socket.close()
 
     def _create_sink(self):
+        """Creates the streaming endpoint as a sink, i.e. an endpoint that is able to receive messages. By doing so, two
+        endpoints are connected and the datastream is opened. If started in multithreading mode, also starts the loop
+        to receive objects and store them in the internal buffer.
+        """
         self._logger.debug("Creating sink...")
         self._endpoint_socket.connect()
         self._logger.debug("Connected to remote endpoint.")
 
         if self._multithreading:
             self._logger.debug("Multithreading enabled.")
-            while self._started:  # FIXME CHECK
+            while self._started:
                 try:
                     p_data = self._endpoint_socket.recv()
                     self._buffer.put(p_data, timeout=10)
-                    self._logger.debug(f"Data received of size {len(p_data)}.")  # FIXME
+                    self._logger.debug(f"Data received of size {len(p_data)}.")
                 except queue.Full:
                     pass
             self._endpoint_socket.close()
