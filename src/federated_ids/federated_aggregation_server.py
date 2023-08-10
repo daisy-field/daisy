d55ca26644cad052208df1fcfb183ce146d5c01f
SJasper <sera300@arcor.de>
2023-08-10 11:25:21 +0200

    Implement aggregation server

--- src/federated_ids/federated_aggregation_server.py
+++ src/federated_ids/federated_aggregation_server.py
@@ -7,100 +7,104 @@
 
 # Optimized server for real-world dataset.
 # Federated learning server which starts the federated learning process on the clients,
-# receives the newly trained client federated_model,
+# receives the newly trained client federated_models,
 # and aggregates them using FedAVG.
 
 
 import logging
+import threading
+from time import sleep
 from typing import Tuple
 
 import src.communication.message_stream as ms
-from models import autoencoder as fm
+from federated_models.federated_model import FederatedModel
+from federated_models.models.autoencoder import FedAutoencoder
+
+from model_aggregation.federated_aggregation import FederatedAggregation
+from model_aggregation.FedAvg.fedavg import FedAvg
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 
-class aggregation_server():
+
+class AggregationServer():
     client_queue = []
 
-    class registration_thread()
+    def __init__(self, addr: Tuple[str, int], federated_model: FederatedModel, aggregation_method: FederatedAggregation):
+        self._addr = addr
+        self._model = federated_model
+        self._aggregation_method = aggregation_method
+        self.client_queue=[]
 
-        def __init__(self, addr: Tuple[str, int]):
-            self._addr = addr
-            threading.Thread.__init__(self)
-
-        def recv_registrations(self):
+    def recv_registrations(self):
             """
-            Receive registration messages on startup
+            Receive registration thread function.
+            Wait for clients to connect and add client StreamEndpoints to client_queue
 
-            :param s:
-            :return:
+            :return: -
             """
             while 1:
-                new_client = ms.StreamEndpoint(self._addr)
-                client_queue.append[new_client]
+                new_client = ms.StreamEndpoint(name="ReceivingEndpoint", addr=self._addr)
+                new_client.start()
+                logging.info("Client registered")
+                self.client_queue.append(new_client)
+                sleep(30)
 
-    def __init__(self, addr: Tuple[str, int], federated_model: fm.FederatedModel):
-        self._addr = addr
-        self.model = federated_model.create_model()
-
-    def start_client_training(client: ms.StreamEndpoint):
+    def start_client_training(self, client: ms.StreamEndpoint):
         """
-        Connect to client, send global weights which start the training
+        Connect to client amd send global weights to start the local training
 
-        :param client_port: Port of the client to send the data to
-        :param server_weights_data: List of the latest server model weights
+        :param client: Clients StreamEndpoint
         :return: -
         """
-        client.send(self.model.get_weights())
+        client.send(self._model.get_model_weights())
 
-    def client_response(self, client):
+    def client_response(self, client: ms.StreamEndpoint):
         """
         Receive updated model weights
 
-        :param s: Socket to receive data on
-        :return: Multiple variables, none if it is a no data or registration message, the client data and metrics otherwise
+        :param client: Socket to receive data on
+        :return: received model weights
         """
-        averaged_sum(client.recv())
+        return client.receive()
 
-    def averaged_sum(self, client_weights):
-        """
-        Calculate fedavg between global weights and new weights
+    def run(self):
+        """Run federated training process:
+            - receive registrations
+            - send out weights to registered clients
+            - receive client weights
+            - aggregate weights
 
-        :param global_weights:
-        :param client_weights:
-        :return:
-        """
-        try:
-            global_weights = self.model.get_weights()
-            for i in range(0, len(global_weights)):
-                for j in range(0, len(global_weights[i])):
-                    global_weights[i][j] = (global_weights[i][j] + client_weights[i][j]) / 2
-
-            self.model.set_weights(global_weights)
-        except:
-            print("Malformed weights received!")
-
-    def federated_training(self):
-        """Conduct federated training, send out weigths to clients and receive local weights
-
-        :return:-
+        :return: -
         """
 
-        t = Thread(target=registration_thread, name="Registration", daemon=True)
-        t.open()
+        training_thread = threading.Thread(target=self.recv_registrations, name="Registration", daemon=True)
+        training_thread.start()
 
         _round = 0
         while 1:
             logging.info(f"START ROUND {_round} ")
 
-            for client in client_queue:
-                start_client_training(client)
-
+            for client in self.client_queue:
+                self.start_client_training(client)
+            logging.info(f'Registered Clients: {self.client_queue}')
             logging.info("Started training on all available clients.\n")
 
-            for client in client_queue:
-                client_response(client)
+            for client in self.client_queue:
+                client_weights = self.client_response(client)
+                aggregates_weights = self._aggregation_method.aggregate(self._model.get_model_weights(), client_weights)
+                self._model.set_model_weights(aggregates_weights)
 
             logging.info("Finished training on all available clients.\n")
+            _round += 1
+            sleep(30)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(asctime)s %(levelname)-8s %(name)-10s %(message)s", datefmt="%Y-%m-%d %H:%M:%S",
+                        level=logging.INFO)
+
+    agg = AggregationServer(("127.0.0.1", 54322), federated_model=FedAutoencoder(), aggregation_method = FedAvg())
+    agg.run()
+
