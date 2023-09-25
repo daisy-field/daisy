09b6d0331ba1ef50d2adb63ae489c8fdcada79c8
SJasper <sera300@arcor.de>
2023-09-25 11:53:26 +0200

    Add new branch pointing to cleanup

---
+++ src/transfer_ids/tfl_client.py
@@ -0,0 +1,10 @@
+#
+#   Created: 2023
+#   Author: Seraphin Zunzer
+#   License: MIT License
+# ---------------------------------------------------------------
+
+
+# TODO: Transfer Learning Client
+# TODO Get GAN size according to computational resources
+# TODO Help other clients improving their models through transfer learning
