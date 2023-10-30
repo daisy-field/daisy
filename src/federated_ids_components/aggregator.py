5eb2d8a4d502d926ea167e49525d7e19e4b2d7e3
Hofmann, Fabian <fabian.hofmann@dai-labor.de>
2023-10-30 16:00:16 +0100

    endpoint initiators now are nonblocking and suport generlic addresses.
    also started working on node, servers for fed learnign, besides adapting endpoint servers accordingly

--- src/federated_ids_components/aggregator.py
+++ src/federated_ids_components/aggregator.py
@@ -1,3 +1,252 @@
+"""
+    Class for Federated Aggregator TODO
+
+    Author: Fabian Hofmann
+    Modified: 27.10.23
+"""
+import logging
+import threading
+from abc import ABC, abstractmethod
+from time import sleep, time
+from typing import Callable, cast, Optional
+
+import numpy as np
+import tensorflow as tf
+
+from src.communication import StreamEndpoint, EndpointServer
+from src.federated_learning import FederatedModel
+
+
+class FederatedOnlineAggregator(ABC):
+    """TODO
+    """
+    _logger: logging.Logger
+
+    _model: FederatedModel
+    _m_lock: threading.Lock
+
+    _aggr_serv: EndpointServer
+
+    _update_interval_t: int
+    _t_last_update: float
+
+    _fed_updater: threading.Thread
+    _started: bool
+
+    def __init__(self, model: FederatedModel, addr: tuple[str, int],
+                 name: str = "", update_interval_t: int = None):
+        """TODO
+
+        :param model: Actual model to be fitted and run predictions alternatingly in online manner.
+        :param name: Name of federated online node for logging purposes.
+        :param addr: TODO
+        :param update_interval_t: Federated updating interval, defined by time; every X seconds, do a sync update.
+        """
+        self._logger = logging.getLogger(name)
+        self._logger.info("Initializing federated online node...")
+
+        self._model = model
+        self._m_lock = threading.Lock()
+
+        self._aggr_serv = EndpointServer(name="AggrServer", addr=addr, multithreading=True)
+
+        self._update_interval_t = update_interval_t
+        self._t_last_update = time()
+
+        self._started = False
+        self._logger.info("Federated online node initialized.")
+
+    def start(self):
+        """Starts the federated online node, along with any underlying endpoints, data sources, and any other object by
+        an extension of this class (see setup()). Non-blocking.
+
+        :raises RuntimeError: If federated online node has already been started.
+        """
+        self._logger.info("Starting federated online node...")
+        if self._started:
+            raise RuntimeError(f"Federated online node has already been started!")
+        self._started = True
+        _try_ops(
+            self._data_source.open,
+            self._eval_serv.start,
+            self._aggr_serv.start,
+            logger=self._logger
+        )
+        self._logger.info("Performing further setup...")
+        self.setup()
+
+        self._loc_learner = threading.Thread(target=self._create_loc_learner, daemon=True)
+        self._loc_learner.start()
+        if not self._sync_mode:
+            self._logger.info("Async learning detected, starting fed learner thread...")
+            self._fed_updater = threading.Thread(target=self.create_async_fed_learner, daemon=True)
+            self._fed_updater.start()
+        self._logger.info("Federated online node started.")
+
+    @abstractmethod
+    def setup(self):
+        """Setup function that must be implemented, called during start(); sets any new internal state variables and
+        objects up used during the federated updating process, both for synchronous and asynchronous learning.
+
+        Note that any such instance attribute should be initialized within an extension of the __init_() method.
+        """
+        raise NotImplementedError
+
+    def stop(self):
+        """Stops the federated online node, along with any underlying endpoints, data sources, and any other object by
+        an extension of this class (see cleanup()).
+
+        :raises RuntimeError: If federated online node has not been started.
+        """
+        self._logger.info("Stopping federated online node...")
+        if not self._started:
+            raise RuntimeError(f"Federated online node has not been started!")
+        self._started = False
+        _try_ops(
+            self._data_source.close,
+            lambda: self._eval_serv.stop(shutdown=True),
+            lambda: self._aggr_serv.stop(shutdown=True),
+            logger=self._logger
+        )
+        self._logger.info("Performing further cleanup...")
+        self.cleanup()
+
+        self._loc_learner.join()
+        if not self._sync_mode:
+            self._logger.info("Async learning detected, waiting for fed learner thread to stop...")
+            self._fed_updater.join()
+        self._logger.info("Federated online node stopped.")
+
+    @abstractmethod
+    def cleanup(self):
+        """Cleanup function that must be implemented, called during stop(); resets any new internal state variables and
+        objects up used during the federated updating process, both for synchronous and asynchronous learning.
+        """
+        raise NotImplementedError
+
+    def _create_loc_learner(self):
+        """Starts the loop to retrieve samples from the data source, arranging them into minibatches and running
+        predictions and fittings on them and the federated model. If set, also initiates synchronous federated update
+        steps if sample/time intervals are satisfied.
+        """
+        self._logger.info(f"AsyncLearner: Starting...")
+        for sample in self._data_source:
+            self._logger.debug(f"AsyncLearner: Appending sample to current minibatch...")
+            self._minibatch_inputs.append(sample[:self._label_split])
+            self._minibatch_labels.append(sample[self._label_split:])
+
+            if len(self._minibatch_inputs) > self._batch_size:
+                self._logger.debug(f"AsyncLearner: Arranging full minibatch for processing...")
+                x_data = np.array(self._minibatch_inputs)
+                y_true = np.array(self._minibatch_labels)
+                with self._m_lock:
+                    try:
+                        self._process_batch(x_data, y_true)
+                    except RuntimeError:
+                        # stop() was called
+                        break
+                self._logger.debug(f"AsyncLearner: Cleaning minibatch window...")
+                self._minibatch_inputs = []
+                self._minibatch_labels = []
+
+                with self._u_lock:
+                    self._s_since_update += self._batch_size
+                if self._sync_mode:
+                    if (self._update_interval_s is not None and self._s_since_update > self._update_interval_s
+                            or self._update_interval_t is not None
+                            and time() - self._t_last_update > self._update_interval_t):
+                        self._logger.debug(f"AsyncLearner: Initiating synchronous federated update step...")
+                        try:
+                            self.sync_fed_update()
+                        except RuntimeError:
+                            # stop() was called
+                            break
+                        self._s_since_update = 0
+                        self._t_last_update = time()
+        self._logger.info(f"AsyncLearner: Stopping...")
+
+    def _process_batch(self, x_data, y_true):
+        """Processes a singular batch for both running a prediction and fitting the federated model around it. Also
+        sends results to both the aggregation and the evaluation server, if available and provided in the beginning.
+
+        :param x_data: Input data.
+        :param y_true: Expected output, for supervised mode and/or evaluation purposes.
+        """
+        self._logger.debug(f"AsyncLearner: Processing minibatch...")
+        y_pred = self._model.predict(x_data)
+        self._logger.debug(f"AsyncLearner: Prediction results for minibatch: {y_pred}")
+        if self._aggr_serv is not None:
+            self._aggr_serv.send((x_data, y_pred))
+
+        if len(self._metrics) > 0:
+            eval_res = [metric(y_true, y_pred) for metric in self._metrics]
+            self._logger.debug(f"AsyncLearner: Evaluation results for minibatch: {eval_res}")
+            if self._eval_serv is not None:
+                self._eval_serv.send(eval_res)
+
+        self._model.fit(x_data, y_true)
+        self._logger.debug(f"AsyncLearner: Minibatch processed...")
+
+    @abstractmethod
+    def sync_fed_update(self):
+        """Singular, synchronous federated update step for the underlying model of the federated online node.
+        Encapsulates all that is necessary, from communication to other nodes, to transferring of one's own model (if
+        necessary) to the model update itself.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_async_fed_learner(self):
+        """Continuous, asynchronous federated update loop, that runs concurrently to the thread of
+        create_local_learner(), to update the underlying model of the federated online node.
+
+        Note that any update of federated models while fitting or prediction is done will result in race conditions and
+        unsafe states! It is therefore crucial to use the _m_lock instance variable to synchronize access to the model.
+        Using this lock object, one can also manage when and how the other thread is able to use the model during any
+        update step (if updating is done in semi-synchronous manner)
+
+        For coordination/planning when to perform an update, any implementation can also use existing state variables
+        also used in the synchronous create_local_learner(), see: _update_interval_s, _update_interval_t,
+        _s_since_update, _t_last_update, for sample- or time-based updating periods. Access to these variables must also
+        be synchronized, using the _u_lock instance variable.
+        """
+        raise NotImplementedError
+
+    def __del__(self):
+        if self._started:
+            self.stop()
+
+class FederatedModelAggregator(FederatedOnlineAggregator):
+    """TODO
+
+    """
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
 #   Created: 2023
 #   Author: Seraphin Zunzer
