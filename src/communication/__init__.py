5bf174abf6bece61721a481d82d7e53c5398a8f0
Hofmann, Fabian <fabian.hofmann@dai-labor.de>
2023-11-02 14:11:13 +0100

    init fixed

--- src/communication/__init__.py
+++ src/communication/__init__.py
@@ -3,11 +3,12 @@
     and LZ4 compression.
 
         * StreamEndpoint - Core class of the communications framework.
-        * EndpointServer -
-
+        * EndpointServer - Helper class to group acceptor endpoints together under one common address.
+        * ep_select()    - Helper function to poll a list of endpoints whether something can be read from/written to.
     Author: Fabian Hofmann
     Modified: 30.10.23
 """
 
 from .message_stream import StreamEndpoint
 from .message_stream import EndpointServer
+from .message_stream import ep_select
