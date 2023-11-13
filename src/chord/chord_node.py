9561a4a3abf1c09fef223421e94238ee36894afd
Lotta Fejzula <lotta.fejzula@outlook.de>
2023-11-13 10:35:01 +0100

    confusing code

--- src/chord/chord_node.py
+++ src/chord/chord_node.py
@@ -55,18 +55,6 @@ class Chordmessage:
         self.node_tuple = node_tuple
 
 
-def send_message(ep, remote_addr, message):
-    if ep is not None:
-        ep.send(message)
-    else:
-        endpoint = StreamEndpoint(name=f"one-time-ep-{np.random.randint(0, 100)}",
-                                  remote_addr=remote_addr, acceptor=False, multithreading=True,
-                                  buffer_size=10000)
-        endpoint.start()
-        endpoint.send(message)
-        endpoint.stop(shutdown=True)
-
-
 # TODO Boostrapping
 # TODO logging
 
@@ -106,12 +94,25 @@ class Chordnode:
         self._id = hash(addr) % (2 ** max_fingers)
         self._name = name
         self._addr = addr
-        self._fingertable = dict.fromkeys(range(0, max_fingers))
+        self._fingertable = {}
         self._successor = successor  # init to None, set at join to chord ring
         self._predecessor = predecessor
         self._node_endpoints = node_endpoints
         self._endpoint_server = EndpointServer(f"{name}-endpointserver", addr=addr, multithreading=True)
         self._max_fingers = max_fingers
+
+    def _send_message(self, ep, remote_addr, message):
+        if ep is not None:
+            if self._clean_up_ep_if_dropout(ep):
+                return
+            ep.send(message)
+        else:
+            endpoint = StreamEndpoint(name=f"one-time-ep-{np.random.randint(0, 100)}",
+                                      remote_addr=remote_addr, acceptor=False, multithreading=True,
+                                      buffer_size=10000)
+            endpoint.start()
+            endpoint.send(message)
+            endpoint.stop(shutdown=True)
 
     def _join(self, remote_addr: tuple[str, int]):
         """
@@ -135,36 +136,35 @@ class Chordnode:
     def _notify(self, remote_id: int, remote_addr: tuple[str, int]):
         notify_message = Chordmessage(message_id=uuid4(), message_type=MessageType.NOTIFY,
                                       node_tuple=self._predecessor)
-        ep = self._check_if_endpoint_exists(remote_id, remote_addr)
-        send_message(ep, remote_addr, notify_message)
-        ep.stop(shutdown=True)
+        ep = self._get_ep_if_exists(remote_id, remote_addr)
+        self._send_message(ep, remote_addr, notify_message)
 
     def _stabilize(self, remote_id: int, remote_addr: tuple[str, int]):
         stabilize_message = Chordmessage(message_id=uuid4(), message_type=MessageType.STABILIZE,
                                          node_tuple=(self._id, self._addr))
-        ep = self._check_if_endpoint_exists(remote_id, remote_addr)
-        send_message(ep, remote_addr, stabilize_message)
-        ep.stop(shutdown=True)
+        ep = self._get_ep_if_exists(remote_id, remote_addr)
+        self._send_message(ep, remote_addr, stabilize_message)
 
     def _update_finger(self, finger_node: tuple[int, tuple[str, int]], finger_id: int):
-        # TODO check logic pp
         finger = self._fingertable.pop(finger_id, None)
         if finger is not None:
             finger_ids_in_ft = {finger_id for finger_id, finger_addr, finger_ep in self._fingertable.values()
                                 if finger[2] is finger_ep}
             if len(finger_ids_in_ft) == 0:
-                self._clean_up_ep(finger[1], finger[2])
+                self._close_and_remove_ep(finger[1], finger[2])
 
-        ep = self._check_if_endpoint_exists(*finger_node)  # checks ep-server and self._finger_table
-        if ep is None:  # create new ep if none exists
+        ep = self._get_ep_if_exists(*finger_node)  # checks ep-server and self._finger_table
+        if ep is not None:
+            if self._clean_up_ep_if_dropout(ep):
+                return
+        else:  # create new ep if none exists
             ep = StreamEndpoint(name=f"finger-ep-{np.random.randint(0, 100)}",
                                 remote_addr=finger_node[1], acceptor=False, multithreading=True,
                                 buffer_size=10000)
             ep.start()
-
         self._fingertable[finger_id] = (finger_node[0], finger_node[1], ep)
 
-    def _check_if_endpoint_exists(self, node_id: int, node_addr: tuple[str, int]) -> Optional[StreamEndpoint]:
+    def _get_ep_if_exists(self, node_id: int, node_addr: tuple[str, int]) -> Optional[StreamEndpoint]:
         ep = {finger_ep for finger_id, finger_addr, finger_ep in self._fingertable.values()
               if node_id == finger_id}
         if len(ep) == 0:
@@ -173,32 +173,45 @@ class Chordnode:
             ep = ep.pop()
         return ep
 
-    def _check_and_clean_up_ep(self, ep: StreamEndpoint) -> bool:
+    def _clean_up_ep_if_dropout(self, ep: StreamEndpoint) -> bool:
         """"
         true if ep is dropout
         """
+        if ep is None:
+            return True
         states, addrs = ep.poll()
-        dropout = not states[0]
-        if dropout:
-            dropouts = [finger_key for finger_key, finger in self._fingertable.items()
-                        if ep is finger[2]]
-            for do in dropouts:
-                do_id, do_addr, do_ep = self._fingertable.pop(do, (None, None, None))
-                if do_ep is None:
-                    continue
-                self._clean_up_ep(do_addr, do_ep)
-
-        if ep is self._predecessor_endpoint:  # TODO check if callbyvalue or callbyreference for ep and is this even necessary?
-            self._clean_up_ep(self._predecessor[1], ep)
-        if ep is self._successor_endpoint:
-            self._clean_up_ep(self._successor[1], ep)
-        return dropout
-
-    def _clean_up_ep(self, ep_addr: tuple[str, int], ep: StreamEndpoint):
-        states, _ = ep.poll()
-        self._endpoint_server.close_connections([ep_addr])
         if states[0]:
-            ep.stop(shutdown=True)  # fixme warnings pp
+            return False
+        dropouts = [finger_key for finger_key, finger in self._fingertable.items()
+                    if ep is finger[2]]
+        for do in dropouts:
+            do_id, do_addr, do_ep = self._fingertable.pop(do, (None, None, None))
+            if do_ep is None:
+                continue
+            self._close_and_remove_ep(do_addr, do_ep)
+        if ep is self._predecessor_endpoint:
+            self._close_and_remove_ep(self._predecessor[1], ep)
+            self._predecessor = None
+            self._predecessor_endpoint = None
+        if ep is self._successor_endpoint:
+            self._close_and_remove_ep(self._successor[1], ep)
+            for finger in range(self._max_fingers):
+                f_id, f_addr, f_ep = self._fingertable.get(finger, (None, None, None))
+                if not self._clean_up_ep_if_dropout(f_ep):
+                    self._successor = (f_id, f_addr)
+                    self._successor_endpoint = f_ep
+                    break
+            if self._successor is None:
+                self._successor = self._predecessor
+                self._successor_endpoint = self._predecessor_endpoint
+        return True
+
+    def _close_and_remove_ep(self, ep_addr: tuple[str, int], ep: StreamEndpoint):
+        ep_in_epserver = self._endpoint_server.get_connections([ep_addr]).get(ep_addr)
+        if ep_in_epserver is None and ep is not None:
+            ep.stop(shutdown=True)
+        else:
+            self._endpoint_server.close_connections([ep_addr])
 
     def _fix_fingers(self):
         """Function to keep fingertables current with nodes joining and leaving the chordring. Should be called
@@ -213,6 +226,7 @@ class Chordnode:
                 message_id = uuid4()
                 self._sent_messages[message_id] = (MessageOrigin.FIX_FINGERS, time(), i)
                 self._find_succ(finger, self._addr, message_id)
+                # fixme only returns succ of fingers, but what if finger doesnt change?
 
     def _is_succ(self, node_id: int) -> bool:
         return (self._id < self._predecessor[0]) & (node_id not in range(self._id + 1, self._predecessor[0] + 1)) \
@@ -222,24 +236,27 @@ class Chordnode:
         return (self._id > self._successor[0]) & (node_id not in range(self._successor[0] + 1, self._id + 1)) \
             or (node_id in range(self._id + 1, self._successor[0] + 1))
 
-    def _get_closest_pred_finger_ep(self, node_id: int) -> StreamEndpoint:
-        ep = None
-        for finger in self._fingertable.keys():
-            f_curr = self._fingertable.get(finger)  # finger_node_id_curr, finger_node_addr_curr, finger_node_ep_curr
-            f_next = self._fingertable.get(
-                finger + 1)  # finger_node_id_next, finger_node_addr_next, finger_node_ep_next
-            if f_curr is None or f_next is None:
-                continue
+    def _get_closest_known_pred(self, node_id: int) -> StreamEndpoint | None:
+        closest_pred = None
+        for finger in range(self._max_fingers):
+            f_curr = self._fingertable.get(finger, None)  # id, addr, ep
+            f_next = self._fingertable.get(finger + 1, None)
+
+            i = finger + 1
+            while (f_curr is None or f_next is None) and i < self._max_fingers:
+                if f_curr is None:
+                    f_curr = self._fingertable.get(i, None)
+                if f_next is None:
+                    f_next = self._fingertable.get(i + 1, None)
+                i += 1
+                if f_curr[0] == f_next[0] and f_curr is not None:
+                    f_next = self._fingertable.get(i, None)
+
             if (f_curr[0] < f_next[0]) & (node_id in range(f_curr[0], f_next[0] + 1)):
-                ep = f_curr[2]
-                break
+                return f_curr[2]  # FIXME
             if (f_curr[0] > f_next[0]) & (node_id in range(f_next[0], f_curr[0] + 1)):
-                ep = f_next[2]
-                break
-        if self._check_and_clean_up_ep(ep):
-            pass
-        # TODO create new ep and return in if
-        return ep
+                return f_next[2]
+        return closest_pred
 
     def _find_succ(self, node_id: int, node_addr: tuple[str, int], message_id: uuid4):
         """Function to find successor of node with chord id find_succ_id. Message will be relayed along the cordring
@@ -253,18 +270,17 @@ class Chordnode:
         if self._is_pred(node_id):
             succ_found_msg = Chordmessage(message_id=message_id, message_type=MessageType.FIND_SUCC_RES,
                                           node_tuple=self._successor)
-            ep = self._check_if_endpoint_exists(node_id, node_addr)
-            send_message(ep, node_addr, succ_found_msg)
-            # Fixme close eps???!!!
+            ep = self._get_ep_if_exists(node_id, node_addr)
+            self._send_message(ep, node_addr, succ_found_msg)  # not closing ep if it comes from check
 
         elif self._is_succ(node_id):
             succ_found_msg = Chordmessage(message_id=message_id, message_type=MessageType.FIND_SUCC_RES,
                                           node_tuple=(self._id, self._addr))
-            ep = self._check_if_endpoint_exists(node_id, node_addr)
-            send_message(ep, node_addr, succ_found_msg)
+            ep = self._get_ep_if_exists(node_id, node_addr)
+            self._send_message(ep, node_addr, succ_found_msg)
         else:
             # idk, ask closest pred of node
-            self._get_closest_pred_finger_ep(node_id).send(
+            self._get_closest_known_pred(node_id).send(
                 Chordmessage(message_id=message_id, message_type=MessageType.FIND_SUCC_REQ,
                              node_tuple=(node_id, node_addr)))
 
@@ -279,6 +295,11 @@ class Chordnode:
         # if (node id is my pred (I am succ) or between me and my pred) update my pred
         if self._is_succ(message.node_tuple[0]):
             self._predecessor = message.node_tuple
+            if self._get_ep_if_exists(*message.node_tuple) is None:
+                self._predecessor_endpoint = StreamEndpoint(
+                    name=f"{self._name}-predecessor-ep-{np.random.randint(0, 100)}",
+                    remote_addr=self._addr, acceptor=False, multithreading=True,
+                    buffer_size=10000)
             # TODO check current pred ep; close old ep and create new one if not exists
         # send notify with my pred
         if message.node_tuple == self._predecessor:
