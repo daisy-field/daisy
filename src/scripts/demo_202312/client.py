0480546d68699aa5bf59c69a3b201e68adfe6d0f
Hofmann, Fabian <fabian.hofmann@dai-labor.de>
2024-01-17 16:57:04 +0100

    started refactoring for scripts and build paths

--- src/scripts/demo_202312/client.py
+++ src/scripts/demo_202312/client.py
@@ -17,32 +17,10 @@ from src.federated_ids_components import FederatedOnlineClient
 from src.federated_learning import TFFederatedModel, FederatedIFTM, EMAvgTM
 
 
-def start_demo_client(client_id: int, pcap_dir_base_path: pathlib.Path, batch_size: int, update_interval: int,
-                      m_aggr_server: tuple[str, int], eval_server: tuple[str, int], aggr_server: tuple[str, int]):
-    """TODO COMMENTS CHECKING
+def parse_args() -> argparse.Namespace:
+    """TODO
 
     """
-    handler = PcapHandler(f"{pcap_dir_base_path}/diginet-cohda-box-dsrc{client_id}")
-    processor = CohdaProcessor(client_id=client_id, events=march23_events)
-    data_source = DataSource(source_handler=handler, data_processor=processor)
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    loss = tf.keras.losses.MeanAbsoluteError()
-    id_fn = TFFederatedModel.get_fae(input_size=65, optimizer=optimizer, loss=loss, batch_size=batch_size, epochs=1)
-    t_m = EMAvgTM(alpha=0.05)
-    err_fn = tf.keras.losses.MeanAbsoluteError(reduction=tf.keras.losses.Reduction.NONE)
-    model = FederatedIFTM(identify_fn=id_fn, threshold_m=t_m, error_fn=err_fn, param_split=65)
-
-    metrics = [ConfMatrSlidingWindowEvaluation(window_size=batch_size * 32)]
-
-    client = FederatedOnlineClient(data_source=data_source, batch_size=batch_size, model=model,
-                                   label_split=65, metrics=metrics,
-                                   m_aggr_server=m_aggr_server, eval_server=eval_server, aggr_server=aggr_server,
-                                   update_interval_t=update_interval)
-    client.start()
-
-
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("--debug", type=bool, default=False,
@@ -74,8 +52,35 @@ if __name__ == "__main__":
     client_options.add_argument("--updateInterval", default=None,
                                 metavar="", help="Federated updating interval, defined by time (s)")
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
+
+def start_demo_client(client_id: int, pcap_dir_base_path: pathlib.Path, batch_size: int, update_interval: int,
+                      m_aggr_server: tuple[str, int], eval_server: tuple[str, int], aggr_server: tuple[str, int]):
+    """TODO COMMENTS CHECKING
+
+    """
+    handler = PcapHandler(f"{pcap_dir_base_path}/diginet-cohda-box-dsrc{client_id}")
+    processor = CohdaProcessor(client_id=client_id, events=march23_events)
+    data_source = DataSource(source_handler=handler, data_processor=processor)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    loss = tf.keras.losses.MeanAbsoluteError()
+    id_fn = TFFederatedModel.get_fae(input_size=65, optimizer=optimizer, loss=loss, batch_size=batch_size, epochs=1)
+    t_m = EMAvgTM(alpha=0.05)
+    err_fn = tf.keras.losses.MeanAbsoluteError(reduction=tf.keras.losses.Reduction.NONE)
+    model = FederatedIFTM(identify_fn=id_fn, threshold_m=t_m, error_fn=err_fn, param_split=65)
+
+    metrics = [ConfMatrSlidingWindowEvaluation(window_size=batch_size * 32)]
+
+    client = FederatedOnlineClient(data_source=data_source, batch_size=batch_size, model=model,
+                                   label_split=65, metrics=metrics,
+                                   m_aggr_server=m_aggr_server, eval_server=eval_server, aggr_server=aggr_server,
+                                   update_interval_t=update_interval)
+    client.start()
+
+def client():
+    args = parse_args()
     if args.debug:
         logging.basicConfig(format="%(asctime)s %(levelname)-8s %(name)-10s %(message)s", datefmt="%Y-%m-%d %H:%M:%S",
                             level=logging.DEBUG)
@@ -94,3 +99,7 @@ if __name__ == "__main__":
     start_demo_client(client_id=args.clientId, pcap_dir_base_path=args.pcapBasePath, batch_size=args.batchSize,
                       update_interval=args.updateInterval,
                       m_aggr_server=m_aggr_serv, eval_server=eval_serv, aggr_server=aggr_serv)
+
+
+if __name__ == "__main__":
+    client()
