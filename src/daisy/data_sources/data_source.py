b75763caf9b7160b6501211d7f7696ed2a7f7c14
Ackerschewski, Jonathan <jonathan.ackerschewski@dai-labor.de>
2024-02-06 13:57:26 +0100

    Added (unfinished) FileRelay

--- src/daisy/data_sources/data_source.py
+++ src/daisy/data_sources/data_source.py
@@ -16,6 +16,7 @@ import queue
 import threading
 from abc import ABC, abstractmethod
 from typing import Callable, Iterator
+from pathlib import Path
 
 import numpy as np
 
@@ -488,6 +489,90 @@ class DataSourceRelay:
             except RuntimeError:
                 # stop() was called
                 break
+        self._logger.info("Data source exhausted, or relay closing...")
+
+    def __del__(self):
+        if self._started:
+            self.stop()
+
+
+class FileRelay:  # TODO add comments
+    _logger: logging.Logger
+
+    _data_source: DataSource
+    _file: Path
+
+    _relay: threading.Thread
+    _started: bool
+
+    def __init__(self, target_file: str, name: str = "", data_source: DataSource = None,
+                 source_handler: SourceHandler = None, generator: Iterator[object] = None,
+                 data_processor: DataProcessor = None, process_fn: Callable[[object], np.ndarray] = lambda o: o,
+                 overwrite_file: bool = False, multithreading: bool = False, buffer_size: int = 1024):
+        self._logger = logging.getLogger(name)
+        self._logger.info("Initializing file relay...")
+
+        if target_file is None or not target_file:
+            raise ValueError("File to write to required.")
+        self._file = Path(target_file)
+        if self._file.is_dir():
+            raise ValueError("File path points to a directory instead of a file.")
+        parent_dir = Path(*self._file.parts[:-1])
+        parent_dir.mkdir(parents=True, exist_ok=True)  # create parent directories
+        try:  # Create the file. If it exists, FileExistsError will be raised. If it is invalid, FileNotFoundError will be raised
+            self._file.touch(exist_ok=False)
+        except FileNotFoundError:
+            raise ValueError("File points at an invalid path.")
+        except FileExistsError:
+            if not overwrite_file:
+                raise ValueError("File already exists and should not be overwritten.")
+
+        if data_source is None:
+            data_source = DataSource(name + "Source", source_handler, generator, data_processor, process_fn,
+                                     multithreading, buffer_size)
+        self._data_source = data_source
+
+        self._started = False
+        self._logger.info("File relay initialized.")
+
+    def start(self):
+        self._logger.info("Starting file relay...")
+        if self._started:
+            raise RuntimeError(f"Relay has already been started!")
+        self._started = True
+        try:
+            self._data_source.open()
+        except RuntimeError:
+            pass
+
+        self._relay = threading.Thread(target=self._create_relay, daemon=True)
+        self._relay.start()
+        self._logger.info("File relay started.")
+
+    def stop(self):
+        self._logger.info("Stopping file relay...")
+        if not self._started:
+            raise RuntimeError(f"Relay has not been started!")
+        self._started = False
+        try:
+            self._data_source.close()
+        except RuntimeError:
+            pass
+
+        self._relay.join()
+        self._logger.info("File relay stopped.")
+
+    def _create_relay(self):
+        self._logger.info("Starting to relay data points from data source...")
+        with self._file.open("b") as file:
+            for d_point in self._data_source:
+                try:
+                    file.write(d_point)  # TODO make this to bytes
+                    # TODO write to file
+                    pass
+                except RuntimeError:
+                    # stop() was called
+                    break
         self._logger.info("Data source exhausted, or relay closing...")
 
     def __del__(self):
