4cac9d1307c21c70fd94f6d4b487a872827fc54c
Jonathan <nattacker@mailbox.org>
2025-01-06 10:36:32 +0100

    fix: using better time to datetime function
    exchanged getting datetime from string format to getting from time_epoch
    added meta.time_epoch to pcap parameters
    
    Signed-off-by: Jonathan <nattacker@mailbox.org>

--- src/daisy/data_sources/network_traffic/pyshark_processor.py
+++ src/daisy/data_sources/network_traffic/pyshark_processor.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 DAI-Labor and others
+# Copyright (C) 2024-2025 DAI-Labor and others
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -33,6 +33,7 @@ from ..data_processor import DataProcessor, flatten_dict
 pcap_f_features = (
     "meta.len",
     "meta.time",
+    "meta.time_epoch",
     "meta.protocols",
     "ip.addr",
     "sll.halen",
