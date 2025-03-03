f177efa50344ad7be2b014e555a683b080c532f8
Ackerschewski, Jonathan <nattacker@mailbox.org>
2025-03-03 11:34:41 +0100

    fix: formatting
    Signed-off-by: Ackerschewski, Jonathan <nattacker@mailbox.org>

---
+++ src/daisy/demos/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2024-2025 DAI-Labor and others
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+Author: Fabian Hofmann, Jonathan Ackerschewski, Seraphin Zunzer
+Modified: 04.11.2024
+"""
--- src/daisy/demos/dsfids.py
+++ src/daisy/demos/dsfids.py
@@ -9,10 +9,6 @@ Author: Jonathan Ackerschewski, Seraphin Zunzer
 Modified: 04.11.2024
 """
 
-import ipaddress
-
-import sys
-
 dsfids_f_features = (
     "meta.len",
     "meta.time",
@@ -83,42 +79,9 @@ dsfids_f_features = (
 )
 
 
-def csv_nn_aggregator(key: str, value: object) -> int:
-    """Simple, exemplary value aggregator. Takes a non-numerical (i.e. string) key-value
-    pair and attempts to converted it into an integer / float. This example does not
-    take the key into account, but only checks the types of the value to proceed. Note,
-    that ipv6 are lazily converted to 32 bit (collisions may occur).
-
-    :param key: Name of pair, which always a string.
-    :param value: Arbitrary non-numerical value to be converted.
-    :return: Converted numerical value.
-    :raises ValueError: If value cannot be converted.
-    """
-
-    if isinstance(value, list):
-        value.sort()
-        return hash(str(value))
-    if isinstance(value, str):
-        try:
-            return int(ipaddress.IPv4Address(value))
-        except ipaddress.AddressValueError:
-            pass
-        try:
-            return int(ipaddress.IPv6Address(value)) % sys.maxsize
-        except ipaddress.AddressValueError:
-            pass
-        try:
-            return int(value, 16)
-        except ValueError:
-            pass
-        return hash(value)
-    raise ValueError(f"Unable to aggregate non-numerical item: {key, value}")
-
-
 def dsfids_label_data_point(d_point: dict) -> dict:
-    """removes the original label key and labels the data points coorectly.
+    """removes the original label key and labels the data points "correctly".
 
-    :param client_id: Client ID.
     :param d_point: Data point as dictionary.
     :return: Labeled data point.
     """
