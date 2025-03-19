1ebab201ad66da560ecdd6199340d2cbf2259d44
Fabian Sebastian Hofmann <fabian@alberts-hofmann.de>
2025-03-19 10:57:20 +0100

    fix: dashboad env variables now point to the correct ones
    also reverted the dockerfile to the old working one, as net tools
    does not belong in production environments
    
    Signed-off-by: Fabian Sebastian Hofmann <fabian@alberts-hofmann.de>

--- src/daisy/scripts/generic_fids_components/dashboard.py
+++ src/daisy/scripts/generic_fids_components/dashboard.py
@@ -30,7 +30,7 @@ def create_dashboard():
 
     os.system(f"python {dashboard_path} makemigrations")
     os.system(f"python {dashboard_path} migrate")
-    dashboard_ip = os.getenv("DASHBOARD_PORT", "0.0.0.0")
+    dashboard_ip = os.getenv("DASHBOARD_IP", "0.0.0.0")
     dashboard_port = os.getenv("DASHBOARD_PORT", "8000")
     os.system(f"python {dashboard_path} runserver {dashboard_ip}:{dashboard_port}")
 
