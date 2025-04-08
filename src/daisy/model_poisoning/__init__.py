3ea2ca60ab712a08121a17a5918646a1aa0b78cd
Ackerschewski, Jonathan <nattacker@mailbox.org>
2025-04-08 14:48:17 +0200

    feat: re-wrote model poisoning
    Signed-off-by: Ackerschewski, Jonathan <nattacker@mailbox.org>

---
+++ src/daisy/model_poisoning/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2024-2025 DAI-Labor and others
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+__all__ = ["ModelPoisoning", "PoisoningMode"]
+
+from model_poisoning import ModelPoisoning, PoisoningMode
--- src/daisy/model_poisoning/model_poisoning.py
+++ src/daisy/model_poisoning/model_poisoning.py
@@ -3,66 +3,88 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-"""Model poisoning scenario, where different untargeted model poisoning attacks can be executed to harm the FL process.
-Depending on the specified poisoning mode, node where model poisoning is enabled either randomizes, zeros or inverts
-the calculated local model weights. When inserted into the FL process, these manipulated models should decrease the
-overall global model performance
+"""Model poisoning scenario, where different untargeted model poisoning attacks can be
+executed to harm the FL process. Depending on the specified poisoning mode, node where
+model poisoning is enabled either randomizes, zeros or inverts the calculated local
+model weights. When inserted into the FL process, these manipulated models should
+decrease the overall global model performance.
 
 Author: Seraphin Zunzer
 Modified: 02.03.25
 """
 
+import logging
+
 import numpy as np
+from enum import Enum
 
 
-def model_poisoning(self, current_params, poisoning_mode, model):  # TODO self???
-    """Function for poisoning model weights. Based on the current parameters and the poisoning mode, the
-    function either returns a list of random, zeroed or inverted parameters with the same shape
-    as the original parameters. If poisoning mode is none, the original parameters are returned.
-    Additionally, the model weights of the nodes local model are modified to the manipulated weights,
-    to be able to visualize the decreasing performance of the manipulated node. This is currently only done
-    for random and zero poisoning.
+class PoisoningMode(Enum):
+    NONE = ("No model poisoning", False, True)
+    ZERO = ("All zeros poisoning", True, False)
+    INVERSE = ("Invert local model", False, True)
+    RANDOM = ("Randomizes local model", True, False)
 
-    :param model: Nodes local detection model, weights are set using the set_weights() function
-    :param current_params: List of current model weights.
-    :param poisoning_mode: Poisoning mode, either None, zeros, random or inverse.
-    :return: poisoned parameters
-    """
+    def __init__(
+        self, description: str, overwrite_local_model: bool, accept_global_model: bool
+    ):
+        self.description = description
+        self.overwrite_local_model = overwrite_local_model
+        self.accept_global_model = accept_global_model
 
-    poisoned_params = []
 
-    if poisoning_mode is None:
-        self._logger.info("Send unpoisoned parameters")
-        return current_params
-    else:
-        if poisoning_mode == "zeros":
-            for layer in current_params:
-                if isinstance(layer, int) or isinstance(layer, float):
-                    poisoned_params.append(0)
-                else:
-                    poisoned_params.append(np.zeros_like(layer))
-            model.set_parameters(poisoned_params)
-            self._logger.info("Model poisoning: Set weights to zero")
+class ModelPoisoning:
+    poisoning_mode: PoisoningMode
 
-        elif poisoning_mode == "random":
-            for layer in current_params:
-                if isinstance(layer, int) or isinstance(layer, float):
-                    poisoned_params.append(np.random.random())
-                else:
-                    poisoned_params.append(np.random.random_sample(layer.shape))
-            model.set_parameters(poisoned_params)
-            self._logger.info("Model poisoning: Set weights random")
+    def __init__(
+        self,
+        poisoning_mode: PoisoningMode,
+        name: str = "ModelPoisoning",
+        log_level: int = None,
+    ):
+        self._logger = logging.getLogger(name)
+        if log_level:
+            self._logger.setLevel(log_level)
 
-        elif self._poisoningMode == "inverse":
-            for layer in current_params:
-                poisoned_params.append(layer * -1)
-            # Skipped setting new parameters, as we need to inverse the real parameters next round
+        self.poisoning_mode = poisoning_mode
 
-        self._logger.info("Poisoned Parameters:")
-        for i in poisoned_params:
-            if not (isinstance(i, int) or isinstance(i, float)):
-                self._logger.info(i.shape)
-            else:
-                self._logger.info(i)
-        self._logger.info("Send poisoned params to model aggregation server...")
-        return poisoned_params
+    def get_poisoned_parameters(self, parameters: list):
+        match self.poisoning_mode:
+            case PoisoningMode.NONE:
+                return parameters
+            case PoisoningMode.ZERO:
+                return self._zero_poisoning(parameters)
+            case PoisoningMode.INVERSE:
+                return self._inverse_poisoning(parameters)
+            case PoisoningMode.RANDOM:
+                return self._random_poisoning(parameters)
+            case _:
+                raise NotImplementedError("Unknown poisoning mode")
+
+    @staticmethod
+    def _zero_poisoning(parameters: list):
+        poisoned_parameters = []
+        for layer in parameters:
+            poisoned_parameters.append(
+                0 if isinstance(layer, (int, float)) else np.zeros_like(layer)
+            )
+        return poisoned_parameters
+
+    @staticmethod
+    def _inverse_poisoning(parameters: list):
+        poisoned_parameters = []
+        for layer in parameters:
+            poisoned_parameters.append(layer * -1)
+        return poisoned_parameters
+
+    @staticmethod
+    def _random_poisoning(parameters: list):
+        poisoned_parameters = []
+        rand = np.random
+        for layer in parameters:
+            poisoned_parameters.append(
+                rand.random()
+                if isinstance(layer, (int, float))
+                else rand.random_sample(layer.shape)
+            )
+        return poisoned_parameters
